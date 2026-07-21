fbdb1e2d55b43040da6fa72f2985143dc6992f50
adityatiwari049 <adityatiwaryman7@gmail.com>
2026-07-21 22:48:30 +0530

    feat(models): Define robust Task dataclass

---
+++ app/runtime/models/task.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from enum import Enum
+from uuid import uuid4
+from runtime.exceptions.task_exceptions import InvalidTaskTitleError
+
+
+
+class Taskstatus(Enum):
+    PENDING = "Pending"
+    RUNNING = "Running"
+    COMPLETED = "Completed"
+    FAILED = "Failed"
+
+class TaskType(Enum):
+    PYTHON = "python"
+    BROWSER = "browser"
+    SHELL = "shell"
+
+
+
+class Task:
+
+    def __init__(self , title: str,task_type: TaskType = TaskType.PYTHON,):
+        self.id = str(uuid4())
+        self.task_type = task_type
+
+        if not title.strip():
+            raise InvalidTaskTitleError()
+        self.title = title
+
+        self.status = Taskstatus.PENDING
+        self.created_at = datetime.now()
+
+    def start(self):
+        self.status = Taskstatus.RUNNING
+    
+    def complete(self):
+        self.status = Taskstatus.COMPLETED
+    
+    def fail(self):
+        self.status = Taskstatus.FAILED
+    
+    def __str__(self):
+        return (
+            f"Task("
+            f"id={self.id}, "
+            f"title='{self.title}', "
+            f"task_type='{self.task_type.value}', "
+            f"status='{self.status.value}', "
+            f"created_at={self.created_at}"
+            f")"
+        )
